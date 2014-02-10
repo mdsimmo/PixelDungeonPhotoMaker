55d8f923cd0467a3ae50a6973faa5bb81c84ad21
mdsimmo <mdsimmo@gmail.com>
2014-02-10 23:15:38 +1100

    added resource file

---
+++ PixelDungeonPhotoMaker/src/imageEditors/ItemCreator.java
@@ -0,0 +1,18 @@
+package imageEditors;
+
+import java.awt.Rectangle;
+
+public class ItemCreator {
+
+	private String asset;
+	private Rectangle selection;
+	
+	public ItemCreator() {
+			
+	}
+	
+	/*public setAsset() {
+		
+	}*/
+	
+}
